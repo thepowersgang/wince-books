f569610624c9544b5ab2378d5238537bbfeba935
John Hodge <tpg@mutabah.net>
2018-10-14 19:51:59 +0800

    Fiddling around, still not linking/compiling

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 export PROJECT_ROOT=$(pwd)
-export PATH=$(PROJECT_ROOT)/ThirdParty/prefix/bin:$PATH
-export XARGO_RUST_SRC=$(PROJECT_ROOT)/deps/rustc-sysroot/src
-export RUST_TARGET_PATH=$(PROJECT_ROOT)
+export PATH=${PROJECT_ROOT}/ThirdParty/prefix/bin:$PATH
+export XARGO_RUST_SRC=${PROJECT_ROOT}/deps/rustc-sysroot/src
+export RUST_TARGET_PATH=${PROJECT_ROOT}
 export CC=arm-mingw32ce-gcc
 
 LD_PRELOAD= CARGO_INCREMENTAL=0 xargo build --target armv4-mingw32ce -v
